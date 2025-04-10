7e4c836acf83a2b95c9a0c48cf7b5e6ac0388f7b
Shreejith Kumar Das S <135417968+ArtsyDEV-C@users.noreply.github.com>
2025-04-10 22:33:43 +0530

    Update favoriteRoutes.js

--- backend/routes/favoriteRoutes.js
+++ backend/routes/favoriteRoutes.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const router = express.Router();
 const favoriteController = require('../controllers/favoriteController');
-const authenticate = require('../middleware/authenticate');
+const { protect } = require('../middleware/authenticate');
 
 // 📥 Add a favorite city
 router.post('/add', authenticate, favoriteController.addFavoriteCity);
